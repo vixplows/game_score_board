ac675ff3be1fe174fc5e78681113bf1988c89a83
vixplows <vicky@vickyplows.co.uk>
2017-06-19 12:16:23 +0100

    Working add results sessions/players but not using :id params hash to properly

--- controllers/sessions_players_controller.rb
+++ controllers/sessions_players_controller.rb
@@ -2,17 +2,19 @@ require('sinatra')
 require('sinatra/contrib/all')
 require_relative('../models/player_session.rb')
 require_relative('../models/result.rb')
+require_relative('../models/session.rb')
 
-# refactored code to use different routing rather than requesting referral path
+# need to refactor code to use different routing rather than requesting referral path so making ues of id params and then also redirect bacj to that specific session/:id as makes more sense to do so!
 get '/sessions/:id/player/new' do
-  @session = Session.find('id')
+  @session_id = request.referer.split('/').last
+  @session = Session.find(@session_id.to_i)
   @game = Game.find(@session.game_id.to_i)
   @results = Result.all()
   @players = Player.all()
   erb(:"sessions_players/new")
 end
 
-post '/sessions/players' do
+post '/sessions/:id' do
   PlayerSession.new(params).save
   redirect to '/sessions'
 end
